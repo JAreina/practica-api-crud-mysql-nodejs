13e446c0f59b755c8ad4cd8fd89211fbd3c18ab2
JAreina <jareinafdez@gmail.com>
2017-11-04 19:43:43 +0100

    actualizar,eliminar

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.get('/agregar', (req,res,next)=>{
 // AGREAGAR A LA BASE DE DATOS POR POST
 app.post('/', (req,res,next) =>{
          req.getConnection((err,conn)=>{
-         	let contacto = {
+         	let usuario= {
                         id:0,
          		clave: req.body.password,
          		nombre: req.body.nombre,
@@ -73,7 +73,7 @@ app.post('/', (req,res,next) =>{
 
          	// ejecutar  el update
          	// 'inser into usuarios SET ?'
-         	conn.query('insert into usuarios.usuario SET ?',contacto, (err,data)=>{
+         	conn.query('insert into usuarios.usuario SET ?',usuario, (err,data)=>{
                                    if(!err){
                                    	res.redirect('/');
                                    	console.log(data   +"éxito al insertar ")
@@ -84,5 +84,66 @@ app.post('/', (req,res,next) =>{
          	})
          })
 });
-app.listen( app.get('port'), () => console.log('Iniciando API CRUD Express con MySQL') );
+
+// consulta para actualizar los datos
+app.get('/editar/:id', (req,res,next)=>{
+	let id = req.params.id;
+	 req.getConnection((err,conn)=>{
+	conn.query('select * from usuario  where id=?',id,(err,data)=>{
+		if(!err){
+			res.render('editar' , {
+				titulo: 'EDITAR USUARIO',
+				data: data
+			});
+		}else{
+			console.log(err);
+		}
+	});
+});
+	});
+
+// EDITAR LOS DATO S
+app.post('/actualizar/:id', (req, res, next) => {
+	req.getConnection((err, conn) => {
+		let usuario = {
+                        id:req.body.id,
+         		clave: req.body.password,
+         		nombre: req.body.nombre,
+         		correo: req.body.correo,
+         		ciudad: req.body.ciudad
+         	};
+
+		conn.query('UPDATE usuario SET ? WHERE id = ?', [usuario, usuario.id],(err, data) => {
+			if(!err) {
+				res.redirect('/');
+			} else {
+				res.redirect('/editar/'+req.body.id);
+			}
+		});
+	});
+});
+
+// eliminar registro de base de datos
+app.post('/eliminar/:id', (req, res, next) => {
+	req.getConnection((err, conn) => {
+		let id = req.params.id;
+
+		conn.query('DELETE FROM usuario WHERE id = ?', id, (err, data) => {
+			if(!err) {
+				res.redirect('/');
+			} else {
+				return next(new Error('Registro no encontrado'));
+			}
+		});
+	});
+});
+
+// manejo de 404
+app.use((req,res)=>{
+	let err = new Error();
+	err.status = 404;
+	err.statusText = "NOT FOUND";
+	res.render('error',{error:err});
+});
+app.listen( app.get('port'), () => console.log('Iniciando API CRUD Express con MySQL en puerto '+port) );
 
